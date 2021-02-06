317fb9d42aea193c5b43c8497973f9af8b4ef96d
Ihor16 <66701064+Ihor16@users.noreply.github.com>
2021-02-06 13:50:49 -0800

    Changed a comment

--- variour_thingies.R
+++ variour_thingies.R
@@ -5,7 +5,7 @@ write_csv(mtcars, "mtcars.csv")
 
 data(mtcars)
 
-## Generate the graph 7
+## Generate the graph 8
 mpg_hp <- mtcars %>%
   ggplot(aes(x = mpg, y = hp, color = factor(cyl))) +
   geom_point() +
