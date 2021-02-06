6910d3defb9605847deafec9b5f6ac58f5ed6a42
Igor <ihor.parkhomenko@alumni.ubc.ca>
2021-02-06 13:25:38 -0800

    Added the testing RScript file

---
+++ variour_thingies.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+install.packages("svglite")
+library(svglite)
+write_csv(mtcars, "mtcars.csv")
+
+data(mtcars)
+
+mpg_hp <- mtcars %>%
+  ggplot(aes(x = mpg, y = hp, color = factor(cyl))) +
+  geom_point() +
+  labs(x = "Miles per galon",
+       y = "Hourse power",
+       color = "Number of cylinders",
+       title = "How does horse power relate to miles per galon") +
+  theme(text = element_text(size = 14))
+
+mpg_hp
+
+ggsave(filename = "mpg_hp.svg", plot = mpg_hp, width = 10, height = 5)
